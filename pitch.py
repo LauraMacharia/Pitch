96102a9077c174a5c1c1a2df44b62f7ac85a1fd6
Laura Macharia <lauramacharia117@gmail.com>
2019-08-04 21:51:38 +0300

    added a nav bar

--- pitch.py
+++ pitch.py
@@ -1,14 +1,26 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
+from flask_bootstrap import Bootstrap
 app = Flask(__name__)
+
+posts = [
+    
+
+    {
+        'author': 'Hans Seyle'
+        'pitch': 'He who has conquered doubt and fear will conquer failure.'
+        'date_posted': 'May 9, 2019'
+    }
+    
+]
 
 @app.route("/")
 @app.route("/home")
 def home():
-    return "<h1>Home Page</hi>"
+    return render_template('home.html' posts=posts)
 
 @app.route("/about")
 def about():
-    return "<h1>About Page</hi>"
+    return render_template('about.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
