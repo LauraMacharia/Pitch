cafaaee19c41f9813ce289728ac7da0bf922d0c7
Laura Macharia <lauramacharia117@gmail.com>
2019-08-05 14:44:45 +0300

    debugged the app

--- forms.py
+++ forms.py
@@ -1,9 +1,9 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, Email, EqualTo
+from wtforms.validators import DataRequired, Email, EqualTo
 
 class RegistrationForm(FlaskForm):
-     username = StringField('Username' validators=[DataRequired(), Lenght(min=2, max=20)])
+     username = StringField('Username', validators=[DataRequired()])
      email = StringField('Email', validators=[DataRequired(), Email()])
      password = PasswordField('password', validators=[DataRequired()])
      confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
@@ -15,4 +15,4 @@ class LoginForm(FlaskForm):
      email = StringField('Email', validators=[DataRequired(), Email()])
      password = PasswordField('password', validators=[DataRequired()])
      remember = BooleanField('Remember Me')
-     submit = SubmitField('Login') 
+     submit = SubmitField('Login')
--- pitch.py
+++ pitch.py
@@ -1,16 +1,22 @@
 from flask import Flask, render_template, url_for, flash, redirect
+from flask_sqlalchemy import SQLAlchemy
 from forms import RegistrationForm, LoginForm
 from flask_bootstrap import Bootstrap
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'd46a72d607a329542dc093c8f1edcc4f'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+
+from models import User, Post
 
 posts = [
     
 
     {
-        'author': 'Hans Seyle'
-        'pitch': 'He who has conquered doubt and fear will conquer failure.'
+        'author': 'Hans Seyle',
+        'title' : 'Quote',
+        'content': 'He who has conquered doubt and fear will conquer failure.',
         'date_posted': 'May 9, 2019'
     }
     
@@ -19,24 +25,26 @@ posts = [
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template('home.html' posts=posts)
+    return render_template('home.html', posts = posts)
 
 @app.route("/about")
 def about():
     return render_template('about.html')
 
-@app.route("/register" methods = ['GET', 'POST'] )
+@app.route("/register", methods = ['GET', 'POST'] )
 def register():
-    form = RegistrationForm
+    form = RegistrationForm()
     if form.validate_on_submit():
-        flash(f'Account created for {form.username.data}'!, 'primary')
+        flash(f'Account created for {form.username.data}!', 'primary')
         return redirect(url_for('home'))
-    return render_template('register.html', titile='Register' form=form)
+    return render_template('register.html', titile='Register', form=form)
 
-@app.route("/login")
+@app.route("/login", methods=['GET', 'POST'])
 def login():
-    form = LoginForm
-    return render_template('login.html', titile='Login' form=form)
+    form = LoginForm()
+    if form.validate_on_submit():
+        return redirect(url_for('home'))    
+    return render_template('login.html', title='Login', form=form)
 
 if __name__ == '__main__':
     app.run(debug=True)
