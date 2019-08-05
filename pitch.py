11b75ab4474135eb9560fe4f7b33f1a047d85d8f
Laura Macharia <lauramacharia117@gmail.com>
2019-08-05 08:06:52 +0300

    created a login and a signup file

--- pitch.py
+++ pitch.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template, url_for
+from forms import RegistrationForm, LoginForm
 from flask_bootstrap import Bootstrap
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'd46a72d607a329542dc093c8f1edcc4f'
 
 posts = [
     
@@ -21,6 +24,16 @@ def home():
 @app.route("/about")
 def about():
     return render_template('about.html')
+
+@app.route("/register")
+def register():
+    form = RegistrationForm
+    return render_template('register.html', titile='Register' form=form)
+
+@app.route("/login")
+def login():
+    form = LoginForm
+    return render_template('login.html', titile='Login' form=form)
 
 if __name__ == '__main__':
     app.run(debug=True)
